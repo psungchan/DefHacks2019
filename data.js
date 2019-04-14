1b8232d4a8fb70ba102ca734d210cc63dd8d094e
pedro pires <pedrop3@uw.edu>
2019-04-14 05:31:38 -0700

    database?

---
+++ data.js
@@ -0,0 +1,22 @@
+import * as fs from 'fs'
+
+var user_data = fs.readFileSync('./userData.txt');
+var posts = fs.readFileSync('./posts.txt');
+
+function addToUserData (pFirstName, pLastName, pEmail, pPassword, pBirth) {
+    nuser = {fname:pFirstName, lname:pLastName,emailLogin:pEmail, password:pPassword, birth:pBirth};
+    nuser.push(nuser);
+    fs.appendFile('usersData.txt', nuser, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+}
+
+function addToPosts (email, value, apr, time) {
+    var npost = {emailLogin:email, ammount:value, rate:apr, nMonths:time};
+    posts.push(npost);
+    fs.appendFile('posts.txt', npost, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+}
